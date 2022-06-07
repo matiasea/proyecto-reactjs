45caaea4614f87889a567755fd03b5691852d147
matiasea <mati.almada02@gmail.com>
2022-06-07 00:12:51 -0300

    0.1

---
+++ my-tienda/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <h1>Mi Tienda Digital</h1>
+      <h3>Almada Matias - React</h3>
+    </div>
+  );
+}
+
+export default App;
